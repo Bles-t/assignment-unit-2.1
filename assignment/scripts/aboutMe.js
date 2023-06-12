ceff702a24921ffb941b71289e5859f6a5650a4c
Adrian Bryant <bryant.adrian94@gmail.com>
2023-06-12 02:37:38 -0500

    all questions done

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -13,7 +13,7 @@ let fullName = "Adrian Bryant";
 console.log(fullName);
 
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
-let luckyNumber = "444";
+let luckyNumber = 2;
 
 // 6 - Console log this sentence, adding in the variables you created above:
 // 'My name is (full name), and I think (lucky number) is a winner!'.
@@ -36,25 +36,46 @@ let pets = 1;
 let friendsPets = 4;
 
 // 11 - Add two pets to your `pets` variable
-
-pets = "dog" + "cats";
+pets = pets + 2
+console.log(pets);
 
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
 
-const allowedPets = 10;
+const allowedPets = 5
 
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!",
 // if it's not true, console log "How about we stay home?"
+if (
+  adventurous === true
+) { console.log("Adventures are great!"); }
+
+else {
+  console.log("How about we stay home?");
+}
 
 
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
+if (
+  luckyNumber === 2 && adventurous === true
+) { console.log("Roll the dice!"); }
 
 // 15 - Write a conditional that console logs "I can have more pets!"
 //if the value of `pets` is less than the value of `allowedPets`,
 // console logs "I have enough pets" if the value of `pets` is equal to the value of `allowedPets`,
 // and console logs "Oh no, I have too many pets!"
 // if the value of `pets` is greater than the value of `allowedPets`.
+if (
+  pets < allowedPets
+) {
+  console.log("I can have more pets!");
+}
+else if (
+  pets === allowedPets
+) { console.log("I have enough pets"); }
+else if (pets > allowedPets) { console.log("Oh no, I have too many pets!"); }
+
+
 
 // STRETCH GOALS:
 
@@ -63,6 +84,10 @@ const allowedPets = 10;
 // assigns the highest value to `mostPets`. There's several possibilities --
 // be sure to think through all the scenarios.
 // console.log `mostPets` after the conditional has run.
+let mostPets = 25
+if (
+  pets 
+)
 
 // 17 - Write a *switch* statement that logs:
 //      "First is the worst" if your lucky number is 1
